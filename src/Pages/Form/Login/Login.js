70cd1e0414828ab7849938e3102ca3ec4062336b
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-04-16 19:37:09 +0600

    Signup and Login with email password complete

--- src/Pages/Form/Login/Login.js
+++ src/Pages/Form/Login/Login.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import SocilaLogin from '../../SocialLogin/SocilaLogin';
+import auth from '../../../firebase.init';
+
 
 const Login = () => {
+    const emailRef = useRef('')
+    const passwordRef = useRef('')
 
     const navigate = useNavigate()
 
     const navigateToSignUp = () => {
         navigate('/signup')
     }
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth)
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        const email = emailRef.current.value
+        const password = passwordRef.current.value
+
+        signInWithEmailAndPassword(email, password)
+        console.log(email, password)
+    }
+
+    if(user){
+        navigate('/')
+    }
+
     return (
         <div className="container mt-5">
             <h4 style={{ color: '#FCF4E0' }} className="text-center">Login with your email</h4>
-            <Form className="container w-50 mt-4">
+            <Form onSubmit={handleSubmit} className="container w-50 mt-4">
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
                 </Form.Group>
                 <div className="d-flex aling-items-center justify-content-center">
                     <Button style={{ backgroundColor: '#E5B429' }} variant="primary" type="submit">
@@ -26,9 +52,9 @@ const Login = () => {
                     </Button>
                 </div>
             </Form>
-            <p className="mt-3 text-center" style={{color: '#E5B429'}}>Are you new here? <span className="text-primary" style={{ cursor: 'pointer', color:'#FCF4E0' }} onClick={navigateToSignUp}>SignUp</span> </p>
-            <p className="mt-2 text-center" style={{color: '#E5B429'}}>Forget your password? <span className="text-primary" style={{ cursor: 'pointer', color:'#FCF4E0' }}>Reset Password</span> </p>
-            <SocilaLogin/>
+            <p className="mt-3 text-center" style={{ color: '#E5B429' }}>Are you new here? <span className="text-primary" style={{ cursor: 'pointer', color: '#FCF4E0' }} onClick={navigateToSignUp}>SignUp</span> </p>
+            <p className="mt-2 text-center" style={{ color: '#E5B429' }}>Forget your password? <span className="text-primary" style={{ cursor: 'pointer', color: '#FCF4E0' }}>Reset Password</span> </p>
+            <SocilaLogin />
         </div>
     );
 };
--- src/Pages/Form/SignUp/SignUp.js
+++ src/Pages/Form/SignUp/SignUp.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import SocilaLogin from '../../SocialLogin/SocilaLogin';
 
 const SignUp = () => {
@@ -9,12 +11,33 @@ const SignUp = () => {
     const navigateToLoging = () => {
         navigate('/login')
     }
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    const handleSignUp = (event) => {
+        event.preventDefault()
+        const name = event.target.name.value
+        const email = event.target.email.value
+        const password = event.target.password.value
+
+        createUserWithEmailAndPassword(email, password)
+    }
+
+    if(user){
+        navigate('/')
+    }
+
     return (
         <div className="container mt-5">
             <h4 style={{ color: '#FCF4E0' }} className="text-center">SignUp Here</h4>
-            <Form className="container w-50 mt-4">
+            <Form onSubmit={handleSignUp} className="container w-50 mt-4">
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="text" name="name" placeholder="Enter email" />
+                    <Form.Control type="text" name="name" placeholder="Enter name" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Control type="email" name="email" placeholder="Enter email" />
@@ -26,7 +49,7 @@ const SignUp = () => {
                 <level htmlFlor='terms'> Accept our terms and conditions </level> <br /> <br />
                 <div className="d-flex aling-items-center justify-content-center">
                     <Button style={{ backgroundColor: '#E5B429' }} variant="primary" type="submit">
-                        Login
+                        SignUp
                     </Button>
                 </div>
             </Form>
